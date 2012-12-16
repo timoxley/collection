2170cc663fe79207bec975b8c376bdeaf07d415f
Tim Oxley <secoif@gmail.com>
2012-12-16 18:45:16 +0800

    Refactor test initialisation.

--- test/index.js
+++ test/index.js
@@ -34,76 +34,60 @@ describe('Collection', function() {
       })
     })
   })
-  it('can add items', function() {
-    var collection = new Collection()
-    var user = {
-      name: 'Tim'
-    }
-    collection.add(user)
-    assert(collection.has(user))
-  })
-  it('can remove items', function() {
-    var collection = new Collection()
-    var user = {
-      name: 'Tim'
-    }
-    collection.add(user)
-    assert(true === collection.has(user))
-    collection.remove(user)
-    assert(false === collection.has(user))
-  })
-  it('can clear all items', function() {
-    var collection = new Collection()
-    var tim = {
-      name: 'Tim'
-    }
-    var bob = {
-      name: 'Bob'
-    }
-    collection.add(tim)
-    collection.add(bob)
-    assert(true === collection.has(tim))
-    assert(true === collection.has(bob))
-    collection.clear()
-    assert.equal(collection.count(), 0)
-    assert(false === collection.has(tim))
-    assert(false === collection.has(bob))
-  })
-
-
-  describe('events', function() {
+  describe('add/remove behaviour', function() {
     var collection, tim, bob
     beforeEach(function() {
       collection = new Collection()
       tim = { name: 'Tim' }
       bob = { name: 'Bob' }
     })
-    it('emits added event when adding items', function(done) {
-      collection.on('added', function(addedUsers) {
-        assert.equal(addedUsers.length, 1)
-        assert.strictEqual(tim, addedUsers[0])
-        done()
-      })
+    it('can add an item', function() {
       collection.add(tim)
+      assert(collection.has(tim))
     })
-    it('emits removed event when removing items', function(done) {
+    it('can remove items', function() {
       collection.add(tim)
-      collection.on('removed', function(removedUsers) {
-        assert.equal(removedUsers.length, 1)
-        assert.strictEqual(tim, removedUsers[0])
-        done()
-      })
+      assert(collection.has(tim))
       collection.remove(tim)
+      assert(false === collection.has(tim))
     })
-    it('emits removed event for each removed item during clear', function(done) {
+    it('can clear all items', function() {
       collection.add(tim)
       collection.add(bob)
-      collection.on('removed', function(removedUsers) {
-        assert.equal(removedUsers.length, 2)
-        assert.deepEqual([tim, bob], removedUsers)
-        done()
-      })
       collection.clear()
+      assert.equal(collection.count(), 0)
+      assert(false === collection.has(tim))
+      assert(false === collection.has(bob))
+    })
+
+    describe('events', function() {
+      it('emits added event when adding items', function(done) {
+        collection.on('added', function(addedUsers) {
+          assert.equal(addedUsers.length, 1)
+          assert.strictEqual(tim, addedUsers[0])
+          done()
+        })
+        collection.add(tim)
+      })
+      it('emits removed event when removing items', function(done) {
+        collection.add(tim)
+        collection.on('removed', function(removedUsers) {
+          assert.equal(removedUsers.length, 1)
+          assert.strictEqual(tim, removedUsers[0])
+          done()
+        })
+        collection.remove(tim)
+      })
+      it('emits removed event for each removed item during clear', function(done) {
+        collection.add(tim)
+        collection.add(bob)
+        collection.on('removed', function(removedUsers) {
+          assert.equal(removedUsers.length, 2)
+          assert.deepEqual([tim, bob], removedUsers)
+          done()
+        })
+        collection.clear()
+      })
     })
   })
 })
