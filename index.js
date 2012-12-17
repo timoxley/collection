a7513232bbef10c8ceb1032f494050706dea2eb7
Tim Oxley <secoif@gmail.com>
2012-12-17 23:15:58 +0800

    Add update/remove multi, add multi and events.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+'use strict'
+
 var Enumerable = require('enumerable')
 var Emitter = require('emitter')
 var type = require('type')
+var _ = require('to-function')
+var get = require('get')
+var clone = require('clone')
 
 module.exports = Collection
 
@@ -13,7 +18,7 @@ module.exports = Collection
 function Collection(items) {
   if (!(this instanceof Collection)) {
     if (Array.isArray(items)) return new Collection(items);
-    return mixin(items)
+    return mixin(items, Collection.prototype)
   }
   this.items = items || []
 }
@@ -33,34 +38,134 @@ Collection.prototype.__iterate__ = function(){
  * Mixin the emitter properties.
  *
  * @param {Object} obj
+ * @param {Object} obj
+ *
  * @return {Object}
  * @api private
  */
 
-function mixin(obj) {
-  for (var key in Emitter.prototype) {
-    obj[key] = Emitter.prototype[key];
+function mixin(target, source) {
+  for (var key in source) {
+    target[key] = source[key]
   }
-  return obj;
+  return target
 }
 
-Collection.prototype.add = function add(items) {
+/**
+ * Add an item or an Array of items to the collection.
+ * Does nothing if the item already exists.
+ *
+ * @param {Object|Array} items item or array of items to add
+ * @return {Collection}
+ * @api public
+ */
+
+Collection.prototype.add = function add(items, fn) {
   if (type(items) !== 'array') {
     items = [items]
   }
   var addedItems = []
-  var self = this
   for (var i = 0; i < items.length; i++) {
-    this.items.push(items[i])
+    var item = items[i]
+    if (!this.has(item)) {
+      this.items.push(item)
+      addedItems.push(item)
+    }
   }
-  this.emit('added', items)
+  if (fn) fn(addedItems, this.items)
+  this.emit('added', addedItems)
   return this
+}
+
+/**
+ * Create a `component/to-function` compatible matcher
+ * Object for matching against collection key.
+ *
+ * Can take a hash containing search term as a key value,
+ * or just the key value to locate.
+ *
+ * @param {Object|String} key
+ * @return {Object} matcher object.
+ * @api private
+ */
+
+function getKeyMatcher(key) {
+  var searchKey = key
+  if (typeof key === 'object') searchKey = get(this.key())(key)
+  var match = Object.create(null)
+  if (!searchKey) return match
+  match[this.key()] = searchKey
+  return match
+}
+
+/**
+ * Get object with supplied `key`, or if no key is set on collection,
+ * return the supplied object if it exists in the collection.
+ *
+ * @param {String} key
+ * @return {Object} object whose key matches `key` or undefined
+ * @api public
+ */
+
+Collection.prototype.get = function(key) {
+  if (!this.key()) {
+    return this.has(key)
+      ? key
+      : undefined
+  }
+  return this.find(getKeyMatcher.call(this, key))
+}
+
+Collection.prototype.update = function(key, update) {
+  var keys = Array.isArray(key)
+    ? key
+    : [key]
+
+  var updatedItems = []
+  var updateContent
+  for (var i = 0; i < keys.length; i++) {
+    var id = keys[i]
+    var item = this.get(id)
+    if (!item) continue
+    updateContent = !!update
+      ? update
+      : id
+    var updatedProperties = Object.create(null)
+    if (this.key()) {
+      updatedProperties[this.key()] = item[this.key()]
+    }
+    for (var prop in updateContent) {
+      if (item[prop] !== updateContent[prop]) {
+        item[prop] = updateContent[prop]
+        updatedProperties[prop] = updateContent[prop]
+      }
+    }
+    console.log('updatedProperties', updatedProperties)
+    updatedItems.push(updatedProperties)
+    console.log("updatedItems", updatedItems)
+  }
+  if (updatedItems.length) this.emit('updated', updatedItems)
+  return
+}
+
+/**
+ * Get value at key, or if no key set, search for objects
+ * matching supplied key.
+ *
+ * @param {String} key
+ * @return {Object} object whose key matches `key` or undefined
+ * @api public
+ */
+
+Collection.prototype.has = function(key) {
+  if (!this.key()) return Enumerable.prototype.has.call(this, key)
+  var match = getKeyMatcher.call(this, key)
+  return this.any(match)
 }
 
 Collection.prototype.clear = function clear() {
   var toRemove = this.items
   this.remove(toRemove)
-  this.emit('cleared', toRemove)
   return this
 }
 
@@ -76,7 +181,14 @@ Collection.prototype.remove = function remove(items) {
       removedItems.push(item)
     }
     return hasItem
-  })
+  }).obj
+
   if (removedItems.length) this.emit('removed', removedItems)
+  if (!this.items.length) this.emit('empty')
   return this
+}
+
+Collection.prototype.key = function key(setKey) {
+  if (!setKey) return this._key
+  this._key = setKey
 }
--- test/index.js
+++ test/index.js
@@ -16,24 +16,31 @@ describe('Collection', function() {
         var collection = new Collection()
         assert(Array.isArray(collection.items))
       })
+
       it('wraps when passed an array', function() {
         var items = []
         var collection = new Collection(items)
         assert.strictEqual(collection.items, items)
       })
     })
+
     describe('use as mixin', function() {
       it('creates new when passed array', function() {
         assert(Collection([]) instanceof Collection)
       })
+
       it('mixes into object', function() {
         var obj = {
           name: 'Stuff'
         }
         assert.equal(Collection(obj).name, obj.name)
+        assert(Collection(obj).clear)
+        assert(Collection(obj).remove)
+        assert(Collection(obj).key)
       })
     })
   })
+
   describe('add/remove behaviour', function() {
     var collection, tim, bob
     beforeEach(function() {
@@ -41,16 +48,97 @@ describe('Collection', function() {
       tim = { name: 'Tim' }
       bob = { name: 'Bob' }
     })
+
     it('can add an item', function() {
       collection.add(tim)
       assert(collection.has(tim))
     })
-    it('can remove items', function() {
+
+    it('can remove items by item reference', function() {
       collection.add(tim)
       assert(collection.has(tim))
       collection.remove(tim)
       assert(false === collection.has(tim))
     })
+
+    it('can remove items by key', function() {
+      collection.key('name')
+      collection.add(tim)
+      collection.remove(tim.name)
+      assert(false === collection.has(tim))
+    })
+
+    it('can add update item by item reference', function() {
+      collection.add(tim)
+      collection.update(tim, {age: 27})
+      assert.equal(collection.get(tim).age, 27)
+    })
+
+    it('can add update item by key', function() {
+      collection.key('name')
+      collection.add(tim)
+      collection.update(tim.name, {age: 27})
+      assert.equal(collection.get(tim).age, 27)
+    })
+
+    it('can add update item by key with single param', function() {
+      collection.key('name')
+      collection.add(tim)
+      collection.update({
+        name: 'Tim',
+        age: 27
+      })
+      assert.equal(collection.get(tim).age, 27)
+    })
+    it('does nothing if trying to update item that does not exist', function() {
+      collection.key('name')
+      collection.add(tim)
+      collection.update({
+        name: 'Bill',
+        age: 27
+      })
+      assert(false === collection.has('Bill'))
+    })
+
+    it('can add update multiple items.', function() {
+      collection.key('name')
+      tim.age = 25
+      bob.age = 25
+      collection.add(tim)
+      collection.add(bob)
+      collection.update([{name: 'Tim', age: 27}, {name: 'Bob', age: 23}])
+      assert.equal(collection.get('Tim').age, 27)
+      assert.equal(collection.get('Bob').age, 23)
+    })
+
+    it('can add update multiple items with single update object', function() {
+      collection.key('name')
+      collection.add(tim)
+      collection.add(bob)
+      collection.update([{name: 'Tim'}, {name: 'Bob'}], {age: 23})
+      assert.equal(collection.get('Tim').age, 23)
+      assert.equal(collection.get('Bob').age, 23)
+    })
+
+    it('ignores updates to objects that are not present', function() {
+      collection.key('name')
+      collection.add(tim)
+      // no bob.
+
+      collection.update([{name: 'Tim'}, {name: 'Bob'}], {age: 23})
+      assert.equal(collection.get('Tim').age, 23)
+      assert.strictEqual(collection.get('Bob'), undefined)
+    })
+    it('does nothing if no updates', function() {
+      collection.key('name')
+      collection.add(tim)
+      collection.add(bob)
+
+      collection.update([{name: 'Tim'}, {name: 'Bob'}], {})
+      assert.deepEqual(collection.get('Tim'), tim)
+      assert.deepEqual(collection.get('Bob'), bob)
+    })
+
     it('can clear all items', function() {
       collection.add(tim)
       collection.add(bob)
@@ -69,6 +157,7 @@ describe('Collection', function() {
         })
         collection.add(tim)
       })
+
       it('emits removed event when removing items', function(done) {
         collection.add(tim)
         collection.on('removed', function(removedUsers) {
@@ -78,6 +167,7 @@ describe('Collection', function() {
         })
         collection.remove(tim)
       })
+
       it('emits removed event for each removed item during clear', function(done) {
         collection.add(tim)
         collection.add(bob)
@@ -87,6 +177,78 @@ describe('Collection', function() {
           done()
         })
         collection.clear()
+      })
+      it('emits empty event when collection is cleared or empty', function(done) {
+        collection.once('empty', function() {
+          collection.once('empty', function() {
+            done()
+          })
+          collection.add(tim)
+          collection.clear()
+        })
+        collection.add(tim)
+        collection.remove(tim)
+      })
+      it('emits update when updating items using key name', function(done) {
+        collection.key('name')
+        collection.once('updated', function(updates) {
+          assert.deepEqual([{
+            name: 'Tim',
+            age: 27
+          }], updates)
+          done()
+        })
+        collection.add(tim)
+        collection.update(tim, {age: 27})
+      })
+    })
+
+    describe('uniqueness constraints', function() {
+      it('does not allow multiple of same object by default', function() {
+        collection.add(tim)
+        collection.add(tim)
+        assert.equal(collection.count(function(){return true}), 1)
+      })
+
+      it('enforces uniqueness based on a given key name', function() {
+        collection.key('name')
+        collection.add(bob)
+        collection.add(tim)
+        collection.add({name: 'Tim', age: 10})
+        assert.equal(collection.count(function(){return true}), 2)
+      })
+
+      it('can get items by their key', function() {
+        collection.key('name')
+        collection.add(tim)
+        collection.add(bob)
+        assert.strictEqual(bob, collection.get(bob.name))
+        assert.strictEqual(tim, collection.get(tim.name))
+      })
+      describe('getting without a key()', function() {
+        it('returns the supplied object if in collection', function() {
+          collection.add(tim)
+          assert.strictEqual(collection.get(tim), tim)
+        })
+        it('returns the supplied object if in collection', function() {
+          collection.get(tim)
+          assert.strictEqual(collection.get(bob), undefined)
+        })
+      })
+
+      it('returns undefined if cannot find item', function() {
+        collection.key('name')
+        collection.add(tim)
+        assert.strictEqual(undefined, collection.get(bob.name))
+      })
+
+      it('uses key() to determine if it already has an item', function() {
+        collection.add(tim)
+        collection.add({name: 'Tim', age: 10})
+        assert(false === collection.has('Tim'))
+
+        collection.key('name')
+        assert(collection.has('Tim'))
       })
     })
   })
